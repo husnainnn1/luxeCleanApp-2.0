67644e76f19333165ac611e0e4534ca0855b35a0
Msohail <msoha001@doc.gold.ac.uk>
2025-05-06 12:32:42 +0100

    for railway updated port: process.env.DB_PORT

--- models/sequelize.js
+++ models/sequelize.js
@@ -1,21 +1,20 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-// Set up Sequelize instance using credentials from .env
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql' // using MySQL for this project
+    port: process.env.DB_PORT, 
+    dialect: 'mysql',
+    logging: false
   }
 );
 
-// Test DB connection to make sure it's working
 sequelize.authenticate()
-  .then(() => console.log('Connected to MySQL using Sequelize!'))
-  .catch(err => console.error('Unable to connect to MySQL:', err));
+  .then(() => console.log(' Connected to MySQL using Sequelize!'))
+  .catch(err => console.error(' Unable to connect to MySQL:', err));
 
-// Exporting so I can use this Sequelize instance in models
 module.exports = sequelize;
